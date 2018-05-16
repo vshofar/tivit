28e782616ca9aa26f3491e9f107a1f5db7896a8a
zack <vitor.shofar@gmail.com>
2018-05-15 21:54:55 -0400

    add 'id' field to serializer class. change url to use 'feira' instead of 'feiras'

--- feiras/serializers.py
+++ feiras/serializers.py
@@ -7,5 +7,5 @@ class FeiraSerializer(serializers.ModelSerializer):
 
     class Meta:
         model   = Feira
-        fields  = ('log', 'lat', 'set_sens', 'area_p', 'cod_dist', 'distrito', 'cod_sub_pref', 'sub_pref', 'regiao5', 
+        fields  = ('id','log', 'lat', 'set_sens', 'area_p', 'cod_dist', 'distrito', 'cod_sub_pref', 'sub_pref', 'regiao5', 
                     'regiao8', 'nome_feira','registro', 'logradouro', 'numero', 'bairro', 'referencia')
--- feiras/urls.py
+++ feiras/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from feiras import views
 
 urlpatterns = [
-    url(r'^feiras/$', views.feira_list),
-    url(r'^feiras/(?P<pk>[0-9]+)/$', views.feira_detail),
+    url(r'^feira/$', views.feira_list),
+    url(r'^feira/(?P<pk>[0-9]+)/$', views.feira_detail),
 ]
